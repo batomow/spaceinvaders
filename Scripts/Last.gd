d80b42e7858e0bb5a2a3ecaa7a8324703a4e6ef6
batomow <carlosa_miranda@outlook.com>
2020-04-19 22:38:26 -0700

    Cleanup
    Cherry pick last commit for demo

--- Scripts/Last.gd
+++
@@ -1,8 +0,0 @@
-extends State
-
-func enter(): 
-    print("last state")
-
-func evaluate(_delta): 
-    if Input.is_key_pressed(KEY_1): 
-        machine.next = machine.states['move']
--- Scripts/substate.gd
+++
@@ -1,8 +0,0 @@
-extends State
-
-func enter(): 
-    print("substate")
-
-func evaluate(_delta): 
-    if Input.is_key_pressed(KEY_6): 
-        machine.next = machine.states['substate2']
