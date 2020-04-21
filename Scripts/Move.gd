9576f4745d7119582bde8b565c8862f4f13162a3
batomow <carlosa_miranda@outlook.com>
2020-04-20 18:26:08 -0700

    adds new move inputs

--- Scripts/Move.gd
+++ Scripts/Move.gd
@@ -7,13 +7,11 @@ func enter():
 	print("moving")
 
 func evaluate(_delta): 
-	direction.x = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
-	direction.y = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))
+	direction.x = int(Input.is_action_pressed("second_right")) - int(Input.is_action_pressed("second_left"))
+	direction.y = int(Input.is_action_pressed("second_down")) - int(Input.is_action_pressed("second_up"))
 	if target.has_method("get_health"):
 		if target.health <= 0: 
 			machine.next = machine.states['dead']
-	
-
 	
 func execute(_delta): 
 	target.position += direction * speed * _delta
