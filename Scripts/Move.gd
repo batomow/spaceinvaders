ed502c85f479d4b7b74d2583adb6599adcd2f32c
batomow <carlosa_miranda@outlook.com>
2020-04-20 18:13:50 -0700

    Apply Stash

--- Scripts/Move.gd
+++ Scripts/Move.gd
@@ -9,8 +9,11 @@ func enter():
 func evaluate(_delta): 
 	direction.x = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
 	direction.y = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))
-	if Input.is_key_pressed(KEY_2): 
-		machine.next = machine.states['trapped']
+	if target.has_method("get_health"):
+		if target.health <= 0: 
+			machine.next = machine.states['dead']
+	
+
 	
 func execute(_delta): 
 	target.position += direction * speed * _delta
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,24 @@
 extends KinematicBody2D
 
 onready var machine: State = get_node('Machine')
+var health: float = 1 setget set_health, get_health
+var attack: float = 1 setget set_attack, get_attack
+var bullets: float = 1
 
 func _process(delta):
 	machine.evaluate(delta)
 
 func _physics_process(delta):
 	machine.execute(delta)
+
+func set_health(amount: float = 0): 
+	health = clamp(health + amount, 0, 1)
+
+func get_health(): 
+	return health
+
+func set_attack(amount: float = 0): 
+	attack = clamp(attack + amount, 0, 1)
+
+func get_attack(): 
+	return attack
